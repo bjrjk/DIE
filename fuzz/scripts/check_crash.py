75e7219c7b861fb0c746bd331b36b12d9d2b7dcf
Jack Ren <bjrjk@qq.com>
2022-01-04 16:45:29 +0800

    Update check_crash

--- fuzz/scripts/check_crash.py
+++ fuzz/scripts/check_crash.py
@@ -1,9 +1,30 @@
 #!/usr/bin/env python3
 # encoding = utf-8
-import os, sys, logging, glob
+import os, sys, logging, glob, json, shutil
 import config
 
-def init():
+class JsonModifier:
+    def __init__(self, json_path):
+        self._json_path = json_path
+
+    def read(self):
+        try:
+            with open(self._json_path, 'r') as f:
+                return json.load(f)
+        except FileNotFoundError as e:
+            return {}
+        except json.JSONDecodeError as e:
+            logging.fatal("The Check-Crash Database is corrupted, Please check!")
+            sys.exit(1)
+
+    def write(self, obj):
+        if os.path.exists(self._json_path):
+            shutil.move(self._json_path, self._json_path + ".bak")
+        with open(self._json_path, 'w') as f:
+            json.dump(obj, f)
+
+
+def init(json_modifier: JsonModifier):
     logging.info("Crash Check Script By Jack Ren.")
     logging.debug(f"DIE_ROOT: {config.DIE_ROOT}")
 
@@ -14,12 +35,25 @@ def init():
     logging.debug(f"Output Dirs: {output_dirs}")
 
     crash_list = glob.glob(os.path.join(config.DIE_ROOT, 'output-*/crashes/*.js'))
-    logging.debug(f"Crash List Size: {len(crash_list)}")
+    logging.debug(f"Initial Crash List Size: {len(crash_list)}")
 
-    return crash_list
+    finished_crash_result = json_modifier.read()
+    for crash_path in finished_crash_result.keys():
+        if crash_list.count(crash_path):
+            crash_list.remove(crash_path)
+
+    return crash_list, finished_crash_result
 
 def main():
-    crash_list = init()
+    json_modifier = JsonModifier(config.CHECK_CRASH_DB_PATH)
+    crash_list, finished_crash_result = init(json_modifier)
+    try:
+        while True:
+            pass
+    finally:
+        json_modifier.write(finished_crash_result)
+        logging.info("Program exited!")
+
 
 if __name__ == '__main__':
     main()
--- fuzz/scripts/config.py
+++ fuzz/scripts/config.py
@@ -3,14 +3,15 @@ import multiprocessing, os, sys, logging
 
 # Constants
 LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
-DIE_ROOT = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".."))
+SCRIPT_DIR_ROOT = os.path.dirname(os.path.abspath(__file__))
+CHECK_CRASH_DB_PATH = os.path.join(SCRIPT_DIR_ROOT, "check_crash.db.json")
+DIE_ROOT = os.path.abspath(os.path.join(SCRIPT_DIR_ROOT, "..", ".."))
 DIE_CORPUS_ROOT = os.path.join(DIE_ROOT, 'DIE-corpus')
 
 # User Configurations
 CPU_COUNT = multiprocessing.cpu_count()
 INTERPRETER_TYPE = "ch" # Available: ch
 INTERPRETER_PATH = os.path.join(DIE_ROOT, 'engines/chakracore-1.11.24/out/Release/ch')
-
 
 # Initialization
 logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
