25afce3ff301fe4a304daed3cd655b03e454786c
Jack Ren <bjrjk@qq.com>
2022-01-04 12:58:28 +0800

    Fixed Mode, Added check_crash.py (partial)

---
+++ fuzz/scripts/check_crash.py
@@ -0,0 +1,19 @@
+#!/bin/sh
+# encoding = utf-8
+import os, sys
+
+DIE_ROOT = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".."))
+
+def main():
+    print("Crash Check Script By Jack Ren.")
+    print("DIE_ROOT:", DIE_ROOT)
+
+    output_dirs = []
+    for d in os.listdir(DIE_ROOT):
+        if d.startswith("output"):
+            output_dirs.append(os.path.join(DIE_ROOT, d))
+
+    print(output_dirs)
+
+if __name__ == '__main__':
+    main()
